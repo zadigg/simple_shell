a2e0e9ec483dbf4f46a5cbaca16cad533fc5ee8c
Bisrat Ubuntu <kiyyakebere@gmail.com>
2022-06-14 20:42:53 +0900

    add

--- .hii.c
+++ .hii.c
@@ -33,7 +33,7 @@ gcc -Wall -Werror -Wextra -pedantic -std=gnu9 *.c -o hsh
 ```
 fd
 ### Output
-df
+
 This program have exact same output as `sh` as well as the exact same error output. The only difference is when it prints an error, the name of the program is equivalent to `argv[0]`.
 
 #### Example of error with `sh`:
